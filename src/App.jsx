c0015d4519958d8e9646acb0bade80398398605b
sarvdny <sarvdnypotfode@gmail.com>
2026-03-15 18:50:56 +0530

    added local json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import NewsCard from "./components/NewsCard";
+import Navbar from "./components/NavBar";
 
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=2f155cbbc91844099627db3c1725b3ed`,
-    )
+    fetch(`/apis/data.json`)
       .then((res) => res.json())
       .then((data) => {
         setData(data.articles);
       });
   }, []);
   return (
-    <div className="NewsComponent">
-      {!data ? (
-        <div className="LoadingScreen">Loading..</div>
-      ) : (
-        data.map((newsItem, idx) => (
-          <NewsCard
-            key={idx}
-            ImageURL={newsItem.urlToImage}
-            Headline={newsItem.title}
-            NewsDescription={newsItem.description}
-            urlToNews={newsItem.url}
-          />
-        ))
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="NewsComponent">
+        {!data ? (
+          <div className="LoadingScreen">Loading..</div>
+        ) : (
+          data.map((newsItem, idx) => (
+            <NewsCard
+              key={idx}
+              ImageURL={newsItem.urlToImage}
+              Headline={newsItem.title}
+              NewsDescription={newsItem.description}
+              urlToNews={newsItem.url}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import "./NavBar.css";
+const Navbar = () => {
+  return (
+    <div className="navBar">
+      <h2>Quick Affairs</h2>
+    </div>
+  );
+};
+export default Navbar;
